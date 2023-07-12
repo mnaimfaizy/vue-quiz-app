6b918ed0b025fd9e4847144a8c52d79027bb14ec
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2023-07-12 14:50:23 +0500

    refactor: deivided the App component to have 2 child components Questions and Results

---
+++ src/components/Questions.vue
@@ -0,0 +1,23 @@
+<template>
+      <div class="questions-ctr">
+          <div class="progress">
+              <div class="bar"></div>
+              <div class="status">1 out of 3 questions answered</div>
+          </div>
+          <div class="single-question" v-for="question in questions" :key="question.q">
+              <div class="question">{{ question.q }}</div>
+              <div class="answers" v-for="answer in question.answers" :key="answer.text">
+                  <div class="answer">{{ answer.text }}</div>
+              </div>
+          </div>
+      </div>
+</template>
+<script>
+export default {
+    name: "Questions",
+    props: ['questions'],
+}
+</script>
+<style lang="en">
+    
+</style>
